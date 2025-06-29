ebb96d1a4f68dd826d05822f2072c4c040011227
Bineru Alwis <bineru@gmail.com>
2025-06-29 10:59:40 +0530

    update

--- code-examples/c++/Bubble_Sort/src/main.cpp
+++ code-examples/c++/Bubble_Sort/src/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <array>
 
 void arrayFunction(int* myArray, int sizeofArray);
 
 int main(){
     std::cout<<"Hello World!"<<std::endl;
 
-    int myArray[5] = {1,2,3,4,5};
+    
 
     int sizeofArray = sizeof(myArray) / sizeof(myArray[0]);
 
